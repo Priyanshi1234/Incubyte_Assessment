fec3884e8fa9e5d35d132ebc6fc2ddc2ef20b4af
Priyanshi Chauhan <priyanshi.chauhan1234@gmail.com>
2025-09-28 16:21:48 +0530

    Case 2: any amount of input to add method

--- src/main/java/org/example/KATA_Calculator.java
+++ src/main/java/org/example/KATA_Calculator.java
@@ -2,19 +2,39 @@ package org.example;
 
 public class KATA_Calculator {
     public static void main(String[] args) {
-        int result = new KATA_Calculator().add("");
-        System.out.println(result);
+        int result = new KATA_Calculator().add("1,2,3,4,5");
+        System.out.println("Simple Input with comma separated sum= " + result);
+
+        int result2 = new KATA_Calculator().addAnyAmountOfNums("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        System.out.println("Input with any amount of numbers= " + result2);
+
+        int result3 = new KATA_Calculator().addAnyAmountOfNums((String[]) null);
+        System.out.println("Input with any amount of numbers= " + result3);
     }
 
     public int add(String input) {
         // Case 1: simple input with comma separated
         int sum = 0;
 
-        // Handle empty input. return 0 if input is empty
-        if (input.isEmpty()) {
+        // Handle empty or null input. return 0 if input is empty
+        if (input == null || input.isEmpty()) {
             return 0;
         }
         for (String number : input.split(",")) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+
+    public int addAnyAmountOfNums(String... input) {
+        // Case 2: Input with any amount of numbers
+        int sum = 0;
+
+        // Handle empty or null input. return 0 if input is empty
+        if (input == null || input.length == 0) {
+            return 0;
+        }
+        for (String number : input) {
             sum += Integer.parseInt(number);
         }
         return sum;
--- src/test/java/KATA_CalculatorTest.java
+++ src/test/java/KATA_CalculatorTest.java
@@ -8,6 +8,7 @@ class KATA_CalculatorTest {
     @Mock
     KATA_Calculator kata_Calculator;
 
+    // Unit test case for simple add method
     @Test
     void addEmptyStringTest() {
         kata_Calculator = new KATA_Calculator();
@@ -30,5 +31,42 @@ class KATA_CalculatorTest {
     void addMultipleNumbersTest() {
         kata_Calculator = new KATA_Calculator();
         assertEquals(15, kata_Calculator.add("1,2,3,4,5"));
+    }
+
+    @Test
+    void addNullInputTest() {
+        kata_Calculator = new KATA_Calculator();
+        assertEquals(0, kata_Calculator.add(null));
+    }
+
+    // Unit test cases for addAnyAmountOfNums method
+    @Test
+    void addAnyAmountOfNumsEmptyInputTest() {
+        kata_Calculator = new KATA_Calculator();
+        assertEquals(0, kata_Calculator.addAnyAmountOfNums());
+    }
+
+    @Test
+    void addAnyAmountOfNumsSingleNumberTest() {
+        kata_Calculator = new KATA_Calculator();
+        assertEquals(7, kata_Calculator.addAnyAmountOfNums("7"));
+    }
+
+    @Test
+    void addAnyAmountOfNumsTwoNumbersTest() {
+        kata_Calculator = new KATA_Calculator();
+        assertEquals(12, kata_Calculator.addAnyAmountOfNums("5", "7"));
+    }
+
+    @Test
+    void addAnyAmountOfNumsMultipleNumbersTest() {
+        kata_Calculator = new KATA_Calculator();
+        assertEquals(55, kata_Calculator.addAnyAmountOfNums("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+    }
+
+    @Test
+    void addAnyAmountOfNumsNullTest() {
+        kata_Calculator = new KATA_Calculator();
+        assertEquals(0, kata_Calculator.addAnyAmountOfNums((String[]) null));
     }
 }
